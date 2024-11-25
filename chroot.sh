daa843b7726b3b21eb35fa7c8045c12e900e0e08
Trivaris <github.frustrate854@passmail.net>
2024-11-25 19:55:38 +0100

    Fix

--- chroot.sh
+++ chroot.sh
@@ -70,4 +70,100 @@ cp /etc/mkinitcpio.conf /etc/mkinitcpio.conf.bak
 # Use sed to insert "encrypt" and "lvm2" between "block" and "filesystems" in the HOOKS line
 sed -i 's/\(HOOKS=\([^\)]*\)block\)/\1 encrypt lvm2/' /etc/mkinitcpio.conf
 
+cat /etc/mkinitcpio.conf
+
+echo "Read the HOOKS line and press Enter to continue..."
+read
+sleep 10
+
+echo "Updated HOOKS line:"
+grep "^HOOKS=" /etc/mkinitcpio.conf
+
+##########################################################################
+
+# Regenerate initramfs for both linux and linux-lts kernels
+echo "Regenerating initramfs for linux and linux-lts kernels..."
+
+mkinitcpio -p linux
+mkinitcpio -p linux-lts
+
+##########################################################################
+
+# Modify /etc/locale.gen to uncomment the desired locales (en_US.UTF-8 and de_DE.UTF-8)
+echo "Modifying /etc/locale.gen to enable 'en_US.UTF-8' and 'de_DE.UTF-8' locales..."
+
+# Uncomment en_US.UTF-8 and de_DE.UTF-8 lines in /etc/locale.gen
+sed -i 's/^#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
+sed -i 's/^#de_DE.UTF-8/de_DE.UTF-8/' /etc/locale.gen
+
+# Verify the modification
+echo "Updated /etc/locale.gen:"
+grep -E "en_US.UTF-8|de_DE.UTF-8" /etc/locale.gen
+
+##########################################################################
+
+# Generate the locales
+echo "Running locale-gen..."
+locale-gen
+
+##########################################################################
+
+# Modify /etc/default/grub to include cryptdevice option
+echo "Modifying /etc/default/grub to include 'cryptdevice=/dev/$partition3:volgroup0'..."
+
+# Use sed to add cryptdevice between loglevel=3 and quiet
+sed -i 's/^\(GRUB_CMDLINE_LINUX_DEFAULT="[^"]*\)/\1 cryptdevice=\/dev\/'$partition3':volgroup0/' /etc/default/grub
+
+# Verify the modification
+echo "Updated GRUB_CMDLINE_LINUX_DEFAULT:"
+grep "^GRUB_CMDLINE_LINUX_DEFAULT=" /etc/default/grub
+
+##########################################################################
+
+# Create the EFI directory and mount the EFI partition
+echo "Creating /boot/EFI and mounting the EFI partition..."
+
+mkdir -p /boot/EFI
+mount "${partition_prefix}1" /boot/EFI
+
+# Install GRUB for UEFI systems
+echo "Installing GRUB for UEFI..."
+if ! grub-install --target=x86_64-efi --bootloader-id=grub_uefi --recheck; then
+    echo "GRUB installation failed. Exiting."
+    exit 1
+fi
+
+
+##########################################################################
+
+# Copy GRUB locale files for English and German
+echo "Copying GRUB locale files..."
+
+# For English locale
+cp /usr/share/locale/en\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/en.mo
+
+# For German locale
+cp /usr/share/locale/de\@quot/LC_MESSAGES/grub.mo /boot/grub/locale/de.mo
+
+##########################################################################
+
+# Regenerate GRUB configuration file
+echo "Regenerating GRUB configuration file..."
+grub-mkconfig -o /boot/grub/grub.cfg
+
+##########################################################################
+
+# Enable necessary system services
+echo "Enabling GDM (GNOME Display Manager) and NetworkManager..."
+systemctl enable gdm
+systemctl enable NetworkManager
+
+##########################################################################
+
+# Exit the chroot environment
+echo "Exiting chroot environment..."
+exit 0
+
+##########################################################################
+
 # Verify the modification
